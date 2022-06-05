e962ef832df5441afabbbd8fba8999739ef2e72d
andrea-everett <andrea.dawn.everett@gmail.com>
2022-06-05 14:35:34 -0400

    update-db

--- server.js
+++ server.js
@@ -14,11 +14,16 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-         connectionString: process.env.DATABASE_URL,
-          ssl: true,
-        }
-    });
-  
+      connectionString: 
+        process.env.DATABASE_URL,
+          ssl: {
+            rejectUnauthorized:false
+          }
+       }
+     });
+
+ 
+
 app.use(cors());
 app.use(bodyParser.json());
 app.get('/',  (req, res) =>{

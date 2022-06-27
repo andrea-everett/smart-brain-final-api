ecca307e322aebccdaa8172c46a16aa64ae860ba
andrea-everett <andrea.dawn.everett@gmail.com>
2022-06-27 14:57:48 -0400

    updates

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: 'postgresql-colorful-89405',
     user: 'postgres',
     password: 'postgres',
     database: 'postgres'
@@ -25,7 +25,6 @@ const db = knex({
   //       rejectUnauthorized:false
   //     }   
   // }
-
 });
 
 const app = express();

389370221df90751eb15622616efe8838fc9c887
andrea-everett <andrea.dawn.everett@gmail.com>
2022-06-29 20:55:41 -0400

    deploy

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})    
 // app.post('/outputs', (req, res) => { outputs.handleApiCall(req, res)})
 
-app.listen( 3000, ()=> {
+app.listen(process.env.PORT || 3000, ()=> {
   console.log(`server is running on port ${process.env.PORT}`);
 })
 
